f9ff189fa9c02d400e24f2daca9719f98b71dc88
Ujjwal <ukarora9@gmail.com>
2024-09-08 16:04:55 +0530

    Initial Commit

---
+++ AirBnB/AirBnBApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBnBApp.swift
+//  AirBnB
+//
+//  Created by Ujjwal Arora on 08/09/24.
+//
+
+import SwiftUI
+
+@main
+struct AirBnBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
